6490f79c7e31de358f08b97e4851e0dac0a21dd0
saket <saket.sahu@geezy.co>
2023-10-30 13:10:31 +0530

    Initial commit. Pushing the skeletal of the project with routes, model and mongo connection

---
+++ models/bookSchema.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const bookSchema = mongoose.Schema({
+        title : {
+            type : String
+        },
+        author : {
+            type : String
+        },
+        summary : {
+            type : String
+        },
+        genre : {
+            type : String,
+            enum : ['Horror', 'Thriller', 'Romantic', 'Comedy', 'Documentary', 'Autobiography', 'Inspirational'],
+        },
+        ageGroup : {
+            type : String,
+            enum : ['Children', 'Adult', 'All-ages'],
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+module.exports = books = mongoose.model('content', bookSchema)
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const connectDB = require('./config/database');
+//const fileUpload = require("express-fileupload");
+const bodyParser = require("express");
+const cors = require('cors')
+const router = express.Router();
+const app = express();
+// Connect Database
+connectDB();
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200, // For legacy browser support
+    methods: "GET, PUT,POST, DELETE, PATCH"
+}
+app.use(cors(corsOptions))
+// Init middleware
+app.use(express.json());
+//app.use(fileUpload());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+//health check api
+router.get('/health', (req, res) => {
+    res.status(200).send('Health Ok');
+});
+
+// Define Routes
+app.use('/api', router);
+app.use('/api/v1/books', require('./routes/api/v1/books'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
